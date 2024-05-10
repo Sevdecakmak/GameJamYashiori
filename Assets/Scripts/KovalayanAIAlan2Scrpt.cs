307fbe11e8c17481d6ea0e58d0c169c6aeec9ef6
burak38ipek <115211530+burak38ipek@users.noreply.github.com>
2024-05-11 00:32:29 +0300

    KovalayanAI sesleri eklendi

---
+++ Assets/Scripts/KovalayanAIAlan2Scrpt.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KovalayanAIAlan2Scrpt : MonoBehaviour
+{
+    AudioSource YaratikSesi;
+    void Start()
+    {
+        YaratikSesi = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            YaratikSesi.Play();
+        }
+    }
+}
