6a24e5ea747b5353697a4b079d5642b9b02f9056
Sevdecakmak <rsevdecakmak@gmail.com>
2024-05-11 15:51:22 +0300

    Puzzle portal scripti

--- Assets/Scripts/PuzzleMove.cs
+++ Assets/Scripts/PuzzleMove.cs
@@ -2,11 +2,17 @@ using UnityEngine;
 
 public class PuzzlePiece : MonoBehaviour
 {
+
+
+    public bool portal_acildi;
     // Parçanın doğru konumu
     public Vector3 dogruKonum;
 
     // Parçanın yerleştirilmesi gereken konum
     //public Vector3 dogruKonumParca1 = new Vector3(8f, 0f, 0f);
+
+    int yerlestirilen_parca = 0;
+    int toplam_parca = 9;
 
 
     bool surukleniyor = false;
@@ -40,12 +46,22 @@ public class PuzzlePiece : MonoBehaviour
         if (mesafe <= 0.5f)
         {
             // Parçayı doğru konumun üzerine yerleştir
+
             transform.position = dogruKonum;
+            sayi_arttir();
         }
         else
         {
             // Parçayı başlangıç konumuna geri götür
             transform.position = baslangicPozisyonu1;
+        }
+    }
+    void sayi_arttir()
+    {
+        yerlestirilen_parca++;
+        if (yerlestirilen_parca == toplam_parca)
+        {
+            portal_acildi = true;
         }
     }
 }
