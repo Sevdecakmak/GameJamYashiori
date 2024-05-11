a87eceb76b1941630797969565a1d14b09f71609
Sevdecakmak <rsevdecakmak@gmail.com>
2024-05-11 19:59:36 +0300

    Clean room

--- Assets/Scripts/DolapDoor.cs
+++ Assets/Scripts/DolapDoor.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class DolapDoor : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float theDistance;
+    public GameObject dolap;
+    //public GameObject actionKey;
+    //public GameObject actionText;
+    //public GameObject hinge;
+    //public AudioSource doorSound;
+    //public GameObject activeCross;
 
-    // Update is called once per frame
+
+
+
     void Update()
     {
-        
+        theDistance = PlayerRay.distanceFromTarget;
+    }
+
+    private void OnMouseOver()
+    {
+        if (theDistance <= 2)
+        {
+            //mouse un üstündeyken
+           /* actionKey.SetActive(true);
+            actionText.SetActive(true);
+            activeCross.SetActive(true);*/
+        }
+        else
+        {
+         /*   actionKey.SetActive(false);
+            actionText.SetActive(false); */
+        }
+
+        if (Input.GetButton("Action"))
+        {
+            if (theDistance <= 2)
+            {
+                this.gameObject.GetComponent<BoxCollider>().enabled = false; //var olmaz
+               // actionKey.SetActive(true);
+               // actionText.SetActive(true);
+                dolap.GetComponent<Animation>().Play("New Animator Controller");
+               // hinge.GetComponent<Animation>().Play("OppositeDoor");
+               // doorSound.Play();
+            }
+        }
+    }
+
+    private void OnMouseExit()
+    {
+
+        //üstünde değilken
+       // actionKey.SetActive(false);
+       // actionText.SetActive(false);
+       //activeCross.SetActive(false);
     }
 }
